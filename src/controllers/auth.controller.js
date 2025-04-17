0294f8247c20a08da172b953bbba794737a2f4b1
khanhvan0502 <89264464+khanhvan0502@users.noreply.github.com>
2025-04-17 22:24:59 +0700

    feature/login-be: Create api login and update api register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,10 +3,22 @@ const { successResponse, errorResponse } = require("../utils/response");
 
 const register = async (req, res) => {
   try {
+    const { username, email, password } = req.body;
+
+    if (!password || password.length < 8 || password.length > 32) {
+      return errorResponse(
+        res,
+        "Password must be between 8 and 32 characters long",
+        400
+      );
+    }
+
     const savedUser = await authService.registerUser(req.body);
+    const { password: _, ...userWithoutPassword } = savedUser.toObject();
+
     return successResponse(
       res,
-      { user: savedUser },
+      { user: userWithoutPassword },
       "User registered successfully",
       201
     );
@@ -16,4 +28,23 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return errorResponse(res, "Email and password are required", 400);
+    }
+
+    const user = await authService.loginUser(email, password);
+
+    if (!user) {
+      return errorResponse(res, "Invalid email or password", 401);
+    }
+    return successResponse(res, user, "Login successful");
+  } catch (error) {
+    console.error(err);
+    return errorResponse(res, "Login failed");
+  }
+};
+
+module.exports = { register, login };
