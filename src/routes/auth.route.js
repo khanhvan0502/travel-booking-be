715f960e4eaee5f61b89e349b84acfb152d76442
khanhvan0502 <89264464+khanhvan0502@users.noreply.github.com>
2025-04-23 22:26:18 +0700

    feature/refresh-token: Created api /refresh-token and update api /register

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const trimRequest = require("../middlewares/trimRequest.middleware");
-const { register, login, logout } = require("../controllers/auth.controller");
+const {
+  register,
+  login,
+  logout,
+  refreshToken,
+} = require("../controllers/auth.controller");
 
 router.post("/register", trimRequest, register);
 router.post("/login", trimRequest, login);
+router.post("/refresh-token", refreshToken);
 router.post("/logout", logout);
 
 module.exports = router;
